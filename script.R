e964b037a8312e80ac4025b803737441ad900a88
Claire Zhang <czhang2718@gmail.com>
2021-06-24 22:29:01 -0400

    upload final R code

--- script.R
+++ script.R
@@ -3,26 +3,30 @@ library(hrbrthemes)
 library(viridis)
 library(forcats)
 
-# Load dataset from github
-data <- read.table("https://github.com/czhang2718/spin-glass/blob/main/small_data.txt", header=TRUE, sep=",")
+# gallery
+data <- read.table("https://raw.githubusercontent.com/czhang2718/spin-glass/main/large_data4.csv", header=FALSE, sep=",")
 data <- data %>%
   gather(key="text", value="value") %>%
   mutate(text = gsub("\\.", " ",text)) %>%
   mutate(value = round(as.numeric(value),0))
-
-# plot
 p <- data %>%
   mutate(text = fct_reorder(text, value)) %>%
   ggplot( aes(x=value, color=text, fill=text)) +
-  geom_histogram(alpha=0.6, binwidth = 5) +
+  geom_histogram(alpha=0.6, binwidth = 1) +
   scale_fill_viridis(discrete=TRUE) +
   scale_color_viridis(discrete=TRUE) +
-  theme_ipsum() +
-  theme(
-    legend.position="none",
-    panel.spacing = unit(0.1, "lines"),
-    strip.text.x = element_text(size = 8)
-  ) +
-  xlab("") +
-  ylab("Assigned Probability (%)") +
   facet_wrap(~text)
+p
+
+
+# individual
+data<- read.table("https://raw.githubusercontent.com/czhang2718/spin-glass/main/large_data.txt", header=FALSE)
+data<- as.data.frame(data)
+
+for(i in 19:60){
+  p <- hist(as.numeric(data[i-18,]), breaks=10)
+  jpeg(paste0("hist", i, ".jpg"))
+  plot(p)
+  dev.off()
+}
+
