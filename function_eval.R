8efcf12acc43bbd6f96903d11dda700d256e0385
Claire Zhang <czhang2718@gmail.com>
2021-07-31 13:44:02 -0400

    finish approx stuff

--- function_eval.R
+++ function_eval.R
@@ -1,24 +1,111 @@
 library(pracma)
+library(numDeriv)
 
-n=20
-c=-3
 
-x=c()
-y=c()
-for(r in 0:4000){
-  x=c(x, r)
-  sig=c*sqrt(n)/(sqrt(2)*erfinv(r/(2^(n-1))-1))
-  if(sig<=0){
-    y=c(y, 0)
-    next
-  }
-  sig=sig*sig*(n-1)/2
-  k=n*(n-1)/2
-  p=(sig^(k/2-1))/(2^(k/2))
-  p=p*exp(-sig/2)/gamma(k/2)
-  y=c(y, p)
+assign("n", 19, envir = .GlobalEnv)
+assign("c", -3, envir = .GlobalEnv)
+
+inv <- function(x){
+  sig=c*sqrt(n)/(sqrt(2)*erfinv(x/(2^(n-1))-1))
+  return(sig^2)
 }
 
-write.csv(y, "y.csv")
+phi <- function(y){
+  if(y==0) return(0)
+  return(2^(n-1)*(1+erf(c*sqrt(n)/sqrt(2*y))))
+}
 
-plot(x, y)
+xc=c()
+yc=c()
+sum=0
+
+for(y in 140000:230000){
+  xc=c(xc, y)
+  
+  p=chi(inv(y))
+  # print(paste("grad", grad(inv, y)))
+  p=p*grad(inv, y)
+  yc=c(yc, p)
+  sum=sum+p
+}
+
+plot(xc, yc)
+
+unscale <- function(x){
+  x*(n-1)/2
+}
+
+scale <- function(y){
+  y*2/(n-1)
+}
+
+chi <- function(x){
+  ans=dchisq(unscale(x), n*(n-1)/2)
+  ans=ans*grad(unscale, x)
+  ans
+}
+
+
+xc=c()
+yc=c()
+for(x in 0:8000){
+  xc=c(xc, x)
+  sig=phi(x)*(n-1)/2
+  k=n*(n-1)/2
+  p=dchisq(sig, k)
+  yc=c(yc, p)
+}
+
+
+
+chi <-function(x){
+  y=c()
+  for(i in x){
+    y=c(y, dchisq(i*(n-1)/2, n*(n-1)/2))
+  }
+  return(y)
+}
+
+# A=integrate(chi, 0, 10000)
+# 
+# 
+# y=c()
+# x=c()
+# for(i in 0:400){
+#   x=c(x, i)
+#   y=c(y, dchisq(i*(n-1)/2, n*(n-1)/2))
+# }
+# plot(x, y)
+# 
+# 
+# 
+# 
+# 
+# 
+# pdf <- function(x){
+#   y<-c()
+#   for(r in x){
+#     x=c(x, r)
+#     sig=c*sqrt(n)/(sqrt(2)*erfinv(r/(2^(n-1))-1))
+#     if(sig<=0){
+#       y=c(y, 0)
+#       next
+#     }
+#     sig=sig*sig
+#     p=(sig^(n/2-1))/(2^(n/2))
+#     p=p*exp(-sig/2)/gamma(n/2)
+#     y=c(y, p)
+#   }
+#   return(y)
+# }
+# 
+# A=integrate(pdf, 0, 10000)
+# 
+# xc=c()
+# yc=c()
+# for(x in 1:400){
+#   y=inv(x)
+#   xc=c(xc, x)
+#   yc=c(yc, y)
+# }
+# plot(xc, yc)
--- overlay.R
+++ overlay.R
@@ -1,29 +1,75 @@
-library(tidyverse)
-library(hrbrthemes)
-library(viridis)
-library(forcats)
-library(plotly)
+library(numDeriv)
+library(MASS)
 
-for(i in 3:3){ # 1:5
-  data <- read.table(paste0("https://raw.githubusercontent.com/czhang2718/spin-glass/main/combined", i, ".csv"), header=TRUE, sep=",")
-  for(j in 18:23){
-    plot <- plot_ly(x = data[[paste0("N", j)]], type = "histogram", nbinsx = 40) %>% layout(title = paste0("n=", j))
-    saveWidget(plot, paste0("plots/c", i, "n", j, ".html"), selfcontained = F, libdir = "lib")
+D=200
+assign("n", 19, envir = .GlobalEnv)
+assign("c", -3, envir = .GlobalEnv)
+inf=10000
+hist_area=0
+assign("f_area", 0, envir = .GlobalEnv)
+
+inv <- function(x){
+  sig=c*sqrt(n)/(sqrt(2)*erfinv(x/(2^(n-1))-1))
+  return(sig^2)
+}
+
+unscale <- function(x){
+  x*(n-1)/2
+}
+
+chi <- function(x){
+  ans=dchisq(unscale(x), n*(n-1)/2)
+  ans=ans*grad(unscale, x)
+  ans
+}
+
+
+pdf <- function(x){
+  pc<-c()
+  for(y in x){
+    p=chi(inv(y))
+    p=p*grad(inv, y)
+    pc=c(pc, p)
+  }
+  pc
+}
+
+calc <- function(xvals){
+  y=c()
+  for(i in xvals){
+    if(i==0){
+      y=c(y, 0)
+      next
+    }
+    d=pdf(i)
+    y=c(y, d)
+  }
+  return(y)
+}
+
+for(n in 18:23){
+  for(c in 1:5){
+    data <- read.table(paste0("https://raw.githubusercontent.com/czhang2718/spin-glass/main/combined", c, ".csv"), header=TRUE, sep=",")
+    x <- data[[paste0("N", n)]]
+    xfit<-seq(0, max(x),length=200)
+    yfit<-calc(xfit)
+    png(file=paste0("plots/overlay/n", n, "c", c, ".png"),
+        width=600, height=350)
+    truehist(x)
+    lines(xfit, yfit, col="blue", lwd=2)
+    lines(density(x))
+    dev.off()
   }
 }
-plot
 
-set.seed(123)
 
-df <- data.frame(x <- rchisq(1000, 5, 10),
-                 group <-  sample(LETTERS[1:5], size = 1000, replace = T))
+truehist(x)
+lines(xfit, yfit, col="blue", lwd=2)
+lines(density(x))
 
-p <- ggplot(df, aes(x)) + 
-  geom_histogram(aes(y = ..density..), alpha = 0.7, fill = "#333333") + 
-  geom_density(fill = "#ff4d4d", alpha = 0.5) +
-  theme(panel.background = element_rect(fill = '#ffffff')) +
-  ggtitle(paste("n=", j))
 
-fig <- ggplotly(p)
 
-fig
+
+integrate(calc, 0, 10000) # 1 :)
+
+
