42c0d2eafc1c3cdfb4411f7546ff5a3c6a461c1e
Claire Zhang <czhang2718@gmail.com>
2021-10-28 11:22:39 -0400

    try some stuff

--- local_min/alg1.cpp
+++ local_min/alg1.cpp
@@ -1,17 +1,54 @@
 // randomized greedy shit (attempt 1) tested in count_local.cpp
 // try L starting strings
 // random permutation 1..n
+// also randomize FLIP
 // k
 
 // global: n, J, rng
 auto randomized=[&]()->int{
 
-    auto check=[&](int )
     map<int, bool> vis; // map<ll, bool> for n>30
-    int L=20;
-    while(L--){
+    vector<int> perm(n);
+    iota(perm.begin(), perm.end(), 1);
+ 
+    std::random_device rd;
+    std::mt19937 g(rd());
+ 
+
+    auto start = std::chrono::system_clock::now();
+    auto end = std::chrono::system_clock::now();
+    int ans=0;
+    while((std::chrono::duration_cast<std::chrono::seconds>(end - start).count() !=10)){
         int mask=uniform_int_distribution<int>(0, (1<<n))(rng);
-        while(check(mask)){}         
-         
+        bool seen=0;
+        while(true){
+            if(vis[mask]){
+                seen=1; break;
+            }
+            bool change=0;
+            std::shuffle(perm.begin(), perm.end(), g);
+            for(int i:perm){
+                if(vis[mask]){
+                    seen=1; break;
+                }
+                vis[mask]=1;
+                int c0=0;
+                int c1=0;
+                for(int j=0; j<n; j++){
+                    if(j==i) continue;
+                    if(((mask&(1<<j))>0)^((mask&(1<<i))>0)) c1+=J[min(i, j)][max(i, j)];
+                    else c0+=J[min(i, j)][max(i, j)];
+                }
+                if(c0>c1){
+                    change=1;
+                    mask^=(1<<i);
+                }
+            }
+            if(!change || seen){
+                break;
+            }
+        }  
+        ans+=!seen;  
     }
+    return ans;
 };
--- local_min/count_local.cpp
+++ local_min/count_local.cpp
@@ -15,7 +15,7 @@ using namespace std;
 
 
 const int N=22;
-const int T=50;
+const int T=1;
 double J[N][N];
 double dp[1<<N];
 int ans[T];
@@ -28,27 +28,34 @@ int main(){
    // freopen("output.txt", "w", stdout);
    cout << fixed;
 
-   for(int n=2; n<=N; n++){
+   for(int n=23; n<=23; n++){
       normal_distribution<double> dist(0, sqrt(2.0/(n-1)));
-      double mean=0;
-      double var=0;
+
+      
+
+      // compare 2 methods
       for(int t=0; t<T; t++){
+         double mean=0;
+         double var=0;
+         double sum=0;
+         double ss=0;
+
+         // create J
+         rep(i, 0, n-1){
+            rep(j, i+1, n-1){
+               J[i][j]=dist(rng);
+               ss+=J[i][j]*J[i][j];
+            }
+         }
+         rep(i, 0, n-1){
+            rep(j, i+1, n-1){
+               J[i][j]*=sqrt((double)n/ss);
+               sum+=J[i][j];
+            }
+         }
+
          auto brute=[&]()->int{
             dp[0]=0;
-            double sum=0;
-            double ss=0;
-            rep(i, 0, n-1){
-               rep(j, i+1, n-1){
-                  J[i][j]=dist(rng);
-                  ss+=J[i][j]*J[i][j];
-               }
-            }
-            rep(i, 0, n-1){
-               rep(j, i+1, n-1){
-                  J[i][j]*=sqrt((double)n/ss);
-                  sum+=J[i][j];
-               }
-            }
             dp[0]=sum;
 
             // 00000 == all -1s
@@ -88,12 +95,70 @@ int main(){
                   }
                }
                // if(ok) cout << bin(m) << " " << dp[m] << " " << __builtin_popcount(m) << '\n';
-               local_min+=ok;
+               // if(ok) cout << m << '\n';
+               local_min+=2*ok;
             }
+            return local_min;
          };
-         auto randomized=[&]()->int{
 
+         auto randomized=[&]()->int{
+             map<int, bool> vis; // map<ll, bool> for n>30
+             vector<int> perm(n);
+             for(int i=0; i<n; i++) perm[i]=i;
+          
+             std::random_device rd;
+             std::mt19937 g(rd());
+          
+
+             int local_min=0;
+             using ClockType = std::chrono::system_clock;
+             auto start = ClockType::now();
+             int k=0;
+             while (std::chrono::duration_cast<std::chrono::seconds>(ClockType::now()-start).count()<1 && k<(1<<n)){
+                 int mask=uniform_int_distribution<int>(0, (1<<n))(rng);
+                 if(vis[mask]) continue;
+                 k++;
+                 // cout << "start with " << mask << '\n';
+                 bool seen=0;
+                 while(true){
+                     bool change=0;
+                     std::shuffle(perm.begin(), perm.end(), g);
+                     for(int i:perm){
+                         double c0=0;
+                         double c1=0;
+                         for(int j=0; j<n; j++){
+                             if(j==i) continue;
+                             if(((mask&(1<<j))>0)^((mask&(1<<i))>0)) c1+=J[min(i, j)][max(i, j)];
+                             else c0+=J[min(i, j)][max(i, j)];
+                             // cout << "J[" << min(i, j) << "][" << max(i, j) << "] = " << J[min(i, j)][max(i, j)] << '\n';
+                         }
+                         // cout << c0 << " " << c1 << '\n';
+                         if(c0>c1){
+                             change=1;
+                             mask^=(1<<i);
+                             if(vis[mask]){
+                                 seen=1; break;
+                             }
+                             else{
+                              vis[mask]=1;
+                              k++;
+                             }
+                         }
+                     }
+                     if(!change || seen){
+                         break;
+                     }
+                 }  
+                 local_min+=!seen;  
+                 if(!seen){
+                  // cout << "minima " << mask << '\n';
+                 }
+             }
+             return local_min;
          };
+         cout << brute() << " " << randomized() << '\n'; 
+         // cout << brute() << '\n';
+         // cout << randomized() << '\n';
       }
    }
 
